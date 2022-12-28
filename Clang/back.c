ed944a779da4b247ec57391697b7242d14a1d107
Amachivine <favour2apenefavour2apene@gmail.com>
2022-12-28 02:24:12 -0800

    Some changes

---
+++ Clang/back.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - making use of backspace, newline and tab
+ * Return - Always 0
+ */
+
+int main()
+{
+	printf("hello\t");
+	printf("world\n");
+	printf("Magnanimous\b\n");
+	return (0);
+}
